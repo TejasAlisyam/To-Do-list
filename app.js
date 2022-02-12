4d4e5a2f4de324a2af3603d71291d7de59a115f5
Tejas Alisyam <tejasalisyam@Tejass-Macbook-Air.local>
2022-02-12 23:27:03 +0530

    this is v1

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { request } = require("express");
+
+const app = express();
+
+var items=[];
+
+app.set("view engine",'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+ 
+app.get("/",function(req,res){
+
+    var today = new Date();
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+
+    res.render("list",{kindOfDay: day , newListItem :items});
+});
+
+app.post("/",function(req,res){
+   var item = req.body.todo;
+   items.push(item);
+    res.redirect("/");
+});
+
+
+app.listen(3000,function(req,res){
+    console.log("server is running on port 3000");
+})
